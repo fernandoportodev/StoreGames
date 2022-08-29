d3342b773a5da169f948e6ebdbaa37cca6dd557a
Fernando Porto <fernandogomes143@gmail.com>
2022-08-29 20:47:23 -0300

    [@fernandoportodev] Create project StoreGames

---
+++ src/main/java/project/com/example/GamesStore/GamesStoreApplication.java
@@ -0,0 +1,13 @@
+package project.com.example.GamesStore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GamesStoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GamesStoreApplication.class, args);
+	}
+
+}
