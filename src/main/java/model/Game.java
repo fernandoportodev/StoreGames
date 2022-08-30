c4969e8b23ba630c6a357b9501cc86ccfadab744
Fernando Porto <fernandogomes143@gmail.com>
2022-08-29 21:12:55 -0300

    @fernandoportodev [Created model Game]

---
+++ src/main/java/model/Game.java
@@ -0,0 +1,53 @@
+package model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.time.LocalDate;
+
+@Entity
+public class Game {
+
+    @Id
+    @GeneratedValue (strategy = GenerationType.IDENTITY)
+    int id;
+
+    String name;
+
+    LocalDate releaseDate;
+
+    Category category;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public LocalDate getReleaseDate() {
+        return releaseDate;
+    }
+
+    public void setReleaseDate(LocalDate releaseDate) {
+        this.releaseDate = releaseDate;
+    }
+
+    public Category getCategory() {
+        return category;
+    }
+
+    public void setCategory(Category category) {
+        this.category = category;
+    }
+}
